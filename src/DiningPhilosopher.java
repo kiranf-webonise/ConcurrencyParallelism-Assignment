c0b71a717511d2c467cb92b713291c1d6cfb7096
kiranf-webonise <kiran.fartale@weboniselab.com>
2018-11-01 19:21:32 +0530

    Added indentations

--- src/DiningPhilosopher.java
+++ src/DiningPhilosopher.java
@@ -2,24 +2,23 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DiningPhilosopher {
-
+	private static final int PHILOSOPHERS = 5;
 	public static void main(String[] args) {
-		Lock forks[] = new ReentrantLock[5];
-
-		for(int i = 0; i<5; i++){
-			forks[i] = new ReentrantLock(); 
+		Lock forks[] = new ReentrantLock[PHILOSOPHERS];
+		for(int count = 0; count<PHILOSOPHERS; count++){
+			forks[count] = new ReentrantLock(); 
 		}
 
-		Thread p1 = new Thread(new Philosopher(forks[4], forks[0], "Philosopher 1 "));
-		Thread p2 = new Thread(new Philosopher(forks[0], forks[1], "Philosopher 2 "));
-		Thread p3 = new Thread(new Philosopher(forks[1], forks[2], "Philosopher 3 "));
-		Thread p4 = new Thread(new Philosopher(forks[2], forks[3], "Philosopher 4 "));
-		Thread p5 = new Thread(new Philosopher(forks[3], forks[4], "Philosopher 5 "));
+		Thread philosopher1 = new Thread(new Philosopher(forks[4], forks[0], "Philosopher 1 "));
+		Thread philosopher2 = new Thread(new Philosopher(forks[0], forks[1], "Philosopher 2 "));
+		Thread philosopher3 = new Thread(new Philosopher(forks[1], forks[2], "Philosopher 3 "));
+		Thread philosopher4 = new Thread(new Philosopher(forks[2], forks[3], "Philosopher 4 "));
+		Thread philosopher5 = new Thread(new Philosopher(forks[3], forks[4], "Philosopher 5 "));
 
-		p1.start();
-		p2.start();
-		p3.start();
-		p4.start();
-		p5.start(); 
-  }
+		philosopher1.start();
+		philosopher2.start();
+		philosopher3.start();
+		philosopher4.start();
+		philosopher5.start();
+	}
 }
